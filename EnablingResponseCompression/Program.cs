4f9885c40a4febb5d29f80cd89b43969196f157a
RogerCodes <rogelioherreradev@gmail.com>
2024-04-17 14:55:33 -0600

    Added 'ResponseCompression' Middleware in 'Program.cs'

--- EnablingResponseCompression/Program.cs
+++ EnablingResponseCompression/Program.cs
@@ -1,7 +1,18 @@
+
+using Microsoft.AspNetCore.ResponseCompression;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Adding Response Compression Here
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+});
 
 var app = builder.Build();
 
@@ -15,6 +26,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+//Using Compression Middleware
+//Placing "UseResponseCompression" before other middleware ensures that the response is compressed before it's sent to the client.
+app.UseResponseCompression();
 
 app.UseRouting();
 
